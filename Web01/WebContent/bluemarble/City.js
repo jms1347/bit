3187433f33c981757584ce67db2987d2bc46dc82
rroye <rroye@naver.com>
2014-10-20 21:36:18 +0900

    d

---
+++ Web01/WebContent/bluemarble/City.js
@@ -0,0 +1,67 @@
+/*function Start(){
+	var name = "Start";
+	var price = 1000;
+}
+
+function CS(){
+	var name = "천수";
+	var price = 2000;
+}
+
+function HC(){
+	var name = "허창";
+	var price = 2000;
+}
+
+function Gold(){
+	var name = "황금열쇠";
+}
+
+function Chensu(){
+	var name = "천수";
+	var price = 2000;
+}*/
+
+
+var cities = new Array[40];
+
+cities[0] = {"name": "Start"};
+cities[1] = {"name": "천수", "price": 200000, "toll" : 50000, "level":1, awner:""};
+cities[2] = {"name": "허창", "price": 600000, "toll" : 150000, "level":1, awner:""};
+cities[3] = {"name": "열쇠"};
+cities[4] = {"name": "여남", "price": 200000, "toll" : 50000, "level":1, awner:""};
+cities[5] = {"name": "평원", "price": 200000, "toll" : 50000, "level":1, awner:""};
+cities[6] = {"name": "진류", "price": 200000, "toll" : 50000, "level":1, awner:""};
+cities[7] = {"name": "동맹"};
+cities[8] = {"name": "업", "price": 600000, "toll" : 150000, "level":1, awner:""};
+cities[9] = {"name": "북평", "price": 300000, "toll" : 80000, "level":1, awner:""};
+cities[10] = {"name": "유배지"};
+cities[11] = {"name": "서주", "price": 300000, "toll" : 80000, "level":1, awner:""};
+cities[12] = {"name": "하비", "price": 200000, "toll" : 50000, "level":1, awner:""};
+cities[13] = new City("북해");
+cities[14] = new City("수춘");
+cities[15] = new City("황금열쇠");
+cities[16] = new City("건업");
+cities[17] = new City("오");
+cities[18] = new City("회계");
+cities[19] = new City("여강");
+cities[20] = new City("이벤트2");
+cities[21] = new City("시상");
+cities[22] = new City("장사");
+cities[23] = new City("동맹");
+cities[24] = new City("완");
+cities[25] = new City("신야");
+cities[26] = new City("양양");
+cities[27] = new City("황금열쇠");
+cities[28] = new City("강릉");
+cities[29] = new City("영안");
+cities[30] = new City("이벤트2");
+cities[31] = new City("운남");
+cities[32] = new City("건녕");
+cities[33] = new City("강주");
+cities[34] = new City("성도");
+cities[35] = new City("자동");
+cities[36] = new City("황금열쇠"); 
+cities[37] = new City("한중");
+cities[38] = new City("낙양");
+cities[39] = new City("장안");
