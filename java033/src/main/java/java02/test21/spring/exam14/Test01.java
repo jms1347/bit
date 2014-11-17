f980b37062b668ca78c6353e2192354172f94db8
rroye <rroye@naver.com>
2014-11-17 17:06:26 +0900

    2014-11-17

--- java033/src/main/java/java02/test21/spring/exam14/Test01.java
+++ java033/src/main/java/java02/test21/spring/exam14/Test01.java
@@ -1,25 +1,20 @@
-package java02.test21.spring.exam13;
+package java02.test21.spring.exam14;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class Test02 {
+public class Test01 {
 
   @SuppressWarnings({ "unused", "resource" })
   public static void main(String[] args) {
     //Car c = new Car();
     ApplicationContext ctx =
         new ClassPathXmlApplicationContext(
-            new String[]{"java02/test21/spring/exam13/application-context02.xml"});
+            new String[]{"java02/test21/spring/exam14/application-context.xml"});
 
-    Car2 c01= (Car2)ctx.getBean("c01");
+    Car c01= (Car)ctx.getBean("car");
     System.out.println(c01);
     
-    Car3 c02= (Car3)ctx.getBean("c02");
-    System.out.println(c02);
-    
-    Car4 c03= (Car4)ctx.getBean("c03");
-    System.out.println(c03);
     
   }
 
